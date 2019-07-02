813fd599b9a2008ff658a4c505cb2ef3e059ad1b
James Newton <james.lexogram@gmail.com>
2019-07-02 11:40:07 +0300

    Restore initial list of cards

--- docs/js/flash.js
+++ docs/js/flash.js
@@ -227,218 +227,218 @@
         , "ru": "Как вас зовут?"
         , "audio": "xxxx.mp3"
         }
-      // , { "en":"My name is..."
-      //   , "ru": "Меня зовут..."
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"Excuse f me"
-      //   , "ru": "Извините"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"What is your inf name?"
-      //   , "ru": "Как тебя зовут?"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I have..."
-      //   , "ru": "У меня есть.."
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"Excuse inf me"
-      //   , "ru": "Извини"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"You f have..."
-      //   , "ru": "У вас есть..."
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"You inf have..."
-      //   , "ru": "У тебя есть..."
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"You inf have money "
-      //   , "ru": "У тебя есть деньги"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I have a job"
-      //   , "ru": "У меня есть работа"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to speak"
-      //   , "ru": "говорить"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"Do you inf have any children?"
-      //   , "ru": "У вас есть дети?"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to work "
-      //   , "ru": "работать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I speak Russian a little bit"
-      //   , "ru": "Я немного говорю по-русски"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to live"
-      //   , "ru": "жить"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I work in Moscow"
-      //   , "ru": "Я работаю в Москве."
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to have lunch"
-      //   , "ru": "обедать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I live in Russia"
-      //   , "ru": "Я живу в России"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to get up"
-      //   , "ru": "вставать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I have lunch at one o'clock"
-      //   , "ru": "Я обедаю в час"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I get up at six in the morning"
-      //   , "ru": "Я встаю в шесть утра"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to have breakfast"
-      //   , "ru": "завтракать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to drink"
-      //   , "ru": "пить"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I have breakfast at home"
-      //   , "ru": "Я завтракаю дома"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I speak with colleagues in English"
-      //   , "ru": "Я говорю с коллегами по-английски"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I like to drink coffee at work "
-      //   , "ru": "Я люблю пить кофе на работе"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I am reading a newspaper"
-      //   , "ru": "Я читаю газету"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to read"
-      //   , "ru": "читать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I am writing a letter"
-      //   , "ru": "Я пишу письмо"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to write"
-      //   , "ru": "писать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I have lunch at a cafe"
-      //   , "ru": "Я обедаю в кафе"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to have lunch"
-      //   , "ru": "обедать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to play football"
-      //   , "ru": "играть в футбол"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to play "
-      //   , "ru": "играть"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"Today I am having dinner at a restaurant"
-      //   , "ru": "Сегодня я ужинаю в ресторане"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to have dinner"
-      //   , "ru": "ужинать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I am having a rest"
-      //   , "ru": "я отдыхаю"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to have a rest"
-      //   , "ru": "отдыхать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"In the evening I watch the news"
-      //   , "ru": "Вечером я смотрю новости"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to watch"
-      //   , "ru": "смотреть"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I listen to music in the car"
-      //   , "ru": "Я слушаю музыку в машине"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to listen"
-      //   , "ru": "слушать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"What does he do at work?"
-      //   , "ru": "Что он делает на работе?"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to do"
-      //   , "ru": "делать"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"It was cold yesterday"
-      //   , "ru": "Вчера было холодно"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I talk with clients by phone"
-      //   , "ru": "Я говорю по телефону с клиентами"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"It's warm today"
-      //   , "ru": "Сегодня тепло"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to be"
-      //   , "ru": "быть"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to cost "
-      //   , "ru": "стоить"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"It will be hot tomorrow"
-      //   , "ru": "Завтра будет жарко"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"At what time?"
-      //   , "ru": "Во сколько?"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"How much does milk cost?"
-      //   , "ru": "Сколько стоит молоко?"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"I buy groceries at a supermarket"
-      //   , "ru": "Я покупаю продукты в супермаркете"
-      //   , "audio": "xxxx.mp3"
-      //   }
-      // , { "en":"to buy"
-      //   , "ru": "покупать"
-      //   , "audio": "xxxx.mp3"
-      //   }
+      , { "en":"My name is..."
+        , "ru": "Меня зовут..."
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"Excuse f me"
+        , "ru": "Извините"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"What is your inf name?"
+        , "ru": "Как тебя зовут?"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I have..."
+        , "ru": "У меня есть.."
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"Excuse inf me"
+        , "ru": "Извини"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"You f have..."
+        , "ru": "У вас есть..."
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"You inf have..."
+        , "ru": "У тебя есть..."
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"You inf have money "
+        , "ru": "У тебя есть деньги"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I have a job"
+        , "ru": "У меня есть работа"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to speak"
+        , "ru": "говорить"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"Do you inf have any children?"
+        , "ru": "У вас есть дети?"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to work "
+        , "ru": "работать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I speak Russian a little bit"
+        , "ru": "Я немного говорю по-русски"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to live"
+        , "ru": "жить"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I work in Moscow"
+        , "ru": "Я работаю в Москве."
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to have lunch"
+        , "ru": "обедать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I live in Russia"
+        , "ru": "Я живу в России"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to get up"
+        , "ru": "вставать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I have lunch at one o'clock"
+        , "ru": "Я обедаю в час"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I get up at six in the morning"
+        , "ru": "Я встаю в шесть утра"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to have breakfast"
+        , "ru": "завтракать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to drink"
+        , "ru": "пить"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I have breakfast at home"
+        , "ru": "Я завтракаю дома"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I speak with colleagues in English"
+        , "ru": "Я говорю с коллегами по-английски"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I like to drink coffee at work "
+        , "ru": "Я люблю пить кофе на работе"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I am reading a newspaper"
+        , "ru": "Я читаю газету"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to read"
+        , "ru": "читать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I am writing a letter"
+        , "ru": "Я пишу письмо"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to write"
+        , "ru": "писать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I have lunch at a cafe"
+        , "ru": "Я обедаю в кафе"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to have lunch"
+        , "ru": "обедать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to play football"
+        , "ru": "играть в футбол"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to play "
+        , "ru": "играть"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"Today I am having dinner at a restaurant"
+        , "ru": "Сегодня я ужинаю в ресторане"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to have dinner"
+        , "ru": "ужинать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I am having a rest"
+        , "ru": "я отдыхаю"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to have a rest"
+        , "ru": "отдыхать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"In the evening I watch the news"
+        , "ru": "Вечером я смотрю новости"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to watch"
+        , "ru": "смотреть"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I listen to music in the car"
+        , "ru": "Я слушаю музыку в машине"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to listen"
+        , "ru": "слушать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"What does he do at work?"
+        , "ru": "Что он делает на работе?"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to do"
+        , "ru": "делать"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"It was cold yesterday"
+        , "ru": "Вчера было холодно"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I talk with clients by phone"
+        , "ru": "Я говорю по телефону с клиентами"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"It's warm today"
+        , "ru": "Сегодня тепло"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to be"
+        , "ru": "быть"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to cost "
+        , "ru": "стоить"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"It will be hot tomorrow"
+        , "ru": "Завтра будет жарко"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"At what time?"
+        , "ru": "Во сколько?"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"How much does milk cost?"
+        , "ru": "Сколько стоит молоко?"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"I buy groceries at a supermarket"
+        , "ru": "Я покупаю продукты в супермаркете"
+        , "audio": "xxxx.mp3"
+        }
+      , { "en":"to buy"
+        , "ru": "покупать"
+        , "audio": "xxxx.mp3"
+        }
       , { "en":"to be situated|to be"
         , "ru": "находиться"
         , "audio": "xxxx.mp3"
